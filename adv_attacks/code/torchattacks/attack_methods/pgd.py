38385b2e7e53794978d48bb6dcc75e21b4ed1e0a
Saqib Azim <azimsaqib10@gmail.com>
2022-07-11 02:03:57 +0000

    added pgd, ffgsm attack methods

--- adv_attacks/code/torchattacks/attack_methods/pgd.py
+++ adv_attacks/code/torchattacks/attack_methods/pgd.py
@@ -75,3 +75,91 @@ class PGD(Attack):
             adv_images = torch.clamp(images + delta, min=0, max=1).detach()
 
         return adv_images
+    
+
+class PGD_CLIP(Attack):
+    r"""
+    PGD in the paper 'Towards Deep Learning Models Resistant to Adversarial Attacks'
+    [https://arxiv.org/abs/1706.06083]
+
+    Distance Measure : Linf
+
+    Arguments:
+        model (nn.Module): model to attack.
+        eps (float): maximum perturbation. (Default: 0.3)
+        alpha (float): step size. (Default: 2/255)
+        steps (int): number of steps. (Default: 40)
+        random_start (bool): using random initialization of delta. (Default: True)
+
+    Shape:
+        - images: :math:`(N, C, H, W)` where `N = number of batches`, `C = number of channels`,        `H = height` and `W = width`. It must have a range [0, 1].
+        - labels: :math:`(N)` where each value :math:`y_i` is :math:`0 \leq y_i \leq` `number of labels`.
+        - output: :math:`(N, C, H, W)`.
+
+    Examples::
+        >>> attack = torchattacks.PGD(model, eps=8/255, alpha=1/255, steps=40, random_start=True)
+        >>> adv_images = attack(images, labels)
+
+    """
+    def __init__(self, model, eps=0.3,
+                 alpha=2/255, steps=40, random_start=True):
+        super().__init__("PGD_CLIP", model)
+        self.eps = eps
+        self.alpha = alpha
+        self.steps = steps
+        self.random_start = random_start
+        self._supported_mode = ['default', 'targeted']
+
+    def forward(self, images, labels, text_features=None):
+        r"""
+        Overridden.
+        """
+        images = images.clone().detach().to(self.device)
+        labels = labels.clone().detach().to(self.device)
+
+        if self._targeted:
+            target_labels = self._get_target_label(images, labels)
+
+#         loss = nn.CrossEntropyLoss()
+
+        adv_images = images.clone().detach()
+
+        if self.random_start:
+            # Starting at a uniformly random point
+            adv_images = adv_images + torch.empty_like(adv_images).uniform_(-self.eps, self.eps)
+            adv_images = torch.clamp(adv_images, min=0, max=1).detach()
+
+        for _ in range(self.steps):
+            adv_images.requires_grad = True
+#             outputs = self.model(adv_images)
+            img_features = self.model.encode_image(adv_images).float()
+            img_features = img_features / torch.linalg.norm(img_features, dim=1, keepdim=True)
+        
+            similarity_map = torch.matmul(img_features, text_features.T)
+            batch_size = similarity_map.shape[0]
+            num_classes = similarity_map.shape[1]
+            
+            batch_loss = 0
+            for i in range(batch_size):
+                for j in range(num_classes):
+                    if j == labels[i]:
+                        batch_loss -= similarity_map[i, j]
+                    else:
+                        batch_loss += similarity_map[i, j]
+
+            # Calculate loss
+#             if self._targeted:
+#                 cost = -loss(outputs, target_labels)
+#             else:
+#                 cost = loss(outputs, labels)
+
+            # Update adversarial images
+            grad = torch.autograd.grad(batch_loss, adv_images,
+                                       retain_graph=False, create_graph=False)[0]
+
+            adv_images = adv_images.detach() + self.alpha*grad.sign()
+            delta = torch.clamp(adv_images - images, min=-self.eps, max=self.eps)
+            adv_images = torch.clamp(images + delta, min=0, max=1).detach()
+
+        return adv_images
+
